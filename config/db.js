4a170e43867e57ba279ee8a7b649d4b993b98bad
Yogesh Mishra <Yogeshrmishra@outlook.com>
2025-04-04 19:47:46 +0530

    added cluster url

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 // MongoDB Connection
 const connectDB = async () => {
     try {
-        mongoose.connect('mongodb://localhost:27017/OMS')
-          .then(() => console.log('✅ Connected to MongoDB'))
-          .catch((err) => console.error('❌ MongoDB Connection Error:', err));
+        mongoose.connect('mongodb+srv://gaurav12:gaurav12@cluster0.xsskxr5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+            .then(() => console.log('✅ Connected to MongoDB'))
+            .catch((err) => console.error('❌ MongoDB Connection Error:', err));
     } catch (err) {
         console.error(err.message);
         process.exit(1); // Exit process with failure

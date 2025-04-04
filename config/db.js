015bcf7c584a2937fc6077fc575c360563891ca9
Yogesh Mishra <Yogeshrmishra@outlook.com>
2025-04-04 23:43:57 +0530

    hi

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 
-// MongoDB Connection
 const connectDB = async () => {
     try {
-        mongoose.connect('mongodb+srv://gaurav12:gaurav12@cluster0.xsskxr5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-            .then(() => console.log('✅ Connected to MongoDB'))
-            .catch((err) => console.error('❌ MongoDB Connection Error:', err));
+        await mongoose.connect('mongodb+srv://yogibaba1207:74488851@ascetic.zjr8s.mongodb.net/?retryWrites=true&w=majority&appName=Ascetic');
+        console.log('✅ Connected to MongoDB');
     } catch (err) {
-        console.error(err.message);
+        console.error('❌ MongoDB Connection Error:', err);
         process.exit(1); // Exit process with failure
     }
 };

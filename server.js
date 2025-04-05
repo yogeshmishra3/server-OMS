ea58bd2d18d348e931fdee3e524f860e725a86f9
Yogesh Mishra <Yogeshrmishra@outlook.com>
2025-04-05 18:00:52 +0530

    update changes

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use('/api/message', messageRoutes);
 // Middleware
 
 app.use(cors({
-  origin: ['https://office-management-system-sigma.vercel.app/', 'http://localhost:3000'],
+  origin: ['https://office-management-system-sigma.vercel.app', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']

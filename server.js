6fa6501bd995a1f2b6a90513f9b8c0b8790ac7d7
Yogesh Mishra <Yogeshrmishra@outlook.com>
2025-04-05 13:34:45 +0530

    remove /

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use('/api/message', messageRoutes);
 // Middleware
 
 app.use(cors({
-  origin: 'https://office-management-system-three.vercel.app/',
+  origin: 'https://office-management-system-three.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
